a008a1ca02fdda22adc9e5f537087484e7f44803
farhanaonna <farhanafiroz@gmail.com>
2020-05-21 22:13:10 -0700

    Models have created.

--- Trainingproject/Myapp/admin.py
+++ Trainingproject/Myapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from Myapp.models import Signup
+from .models import Signup, Session, Student
 
 # Register your models here.
 admin.site.register(Signup)
+admin.site.register(Session)
+admin.site.register(Student)
 
--- Trainingproject/Myapp/models.py
+++ Trainingproject/Myapp/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
-
 # Create your models here.
 # name of table-->model(class)
 # fields of my table--> attributes of class
+
 
 class Signup(models.Model):
     username = models.CharField(max_length=40)
     email = models.EmailField(max_length=50)
     password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.username
+
+class Session(models.Model):
+    session=models.CharField(max_length=40, unique=True)
+
+    def __str__(self):
+        return self.session
+class Student(models.Model):
+    name=models.CharField(max_length=20, unique=False)
+    session=models.ForeignKey("session", on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
