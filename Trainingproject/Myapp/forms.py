62b6b585eeb77809180ab0457be8a0093715e5bc
farhanaonna <farhanafiroz@gmail.com>
2020-05-28 18:29:35 -0700

    I have added some more functions in registration form.

--- Trainingproject/Myapp/forms.py
+++ Trainingproject/Myapp/forms.py
@@ -1,7 +1,20 @@
 from django import forms
+from django.core import validators
+
+#def check(value):
+    #if value[0].lower()!='a':
+    #raise forms.ValidationError("Please start a name with a")
+
+def valid(password):
+    if len(password) < 6:
+        raise forms.ValidationError("Please enter more than 6 characters")
+
 
 class Registerform(forms.Form):
     first_name=forms.CharField(label="Enter your first name")
     last_name = forms.CharField(label="Enter your last name")
     email= forms.EmailField(max_length=30)
+    verify_email= forms.EmailField(label="Repeat E-mail")
     comment= forms.CharField(widget=forms.Textarea)
+    password= forms.CharField(widget=forms.PasswordInput, validators=[valid])
+
--- Trainingproject/Myapp/views.py
+++ Trainingproject/Myapp/views.py
@@ -5,11 +5,21 @@ from.import forms
 
 #def home(request):
 #return HttpResponse("Hey look! This is my first web page.")
+from .forms import Registerform
+
 
 def home(request):
     return render(request,"Myapp/home.html")
 
+
 def form_view(request):
-    if request.method:
-        form=forms.Registerform
-    return render(request,"Myapp/index.html", {'form':form})
+    form = forms.Registerform
+    if request.method == "POST":
+        form = forms.Registerform(request.POST)
+        if form.is_valid():
+            print("validation worked")
+            print("name: " + form.cleaned_data['first_name'])
+            print("last_name: " + form.cleaned_data['last_name'])
+            print("email: " + form.cleaned_data['email'])
+
+    return render(request,"Myapp/index.html", {'form': form})
